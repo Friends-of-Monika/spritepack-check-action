ab8c6bf439326c465211a9a6316e5478bcc1f574
dreamscached <dreamscache.d@gmail.com>
2022-07-13 12:53:57 +0200

    Adapt for spritepacks

--- steps/run-checks.sh
+++ steps/run-checks.sh
@@ -3,39 +3,29 @@
 ## Parameters
 
 gh_action_path="$1"
-submod_path="$2"
+spritepack_path="$2"
 
-if [ -z "$submod_path" ]; then
-    submod_path="$(dirname "$(find . -iname "*.rpy" -print -quit)")"
+if [ -z "$spritepack_path" ]; then
+    spritepack_path="."
 fi
 
 
 ## Directory structure
 
 mas="$gh_action_path/mas"
-mod="$mas/game/Submods"
 renpy="$gh_action_path/renpy"
 
 
 ## Checks run
 
-mkdir -p "$mod"
+find "$spritepack_path" \
+    -type d \
+    \( -path "*/game" -o \
+    -path "*/mod_assets" -o \
+    -path "*/characters" \) \
+    -prune \
+    -exec sh -c 'cd "$(dirname "$0")"; cp -rv "$(basename "$(printf "%s" "$0" | sed "s/game\/mod_assets$/game/")")" "$1"' \{\} "$mas" \;
 
-#shellcheck disable=SC2164
-(cd "$submod_path"; find . -iname "*.rpy" -exec cp -r --parents \{\} "$mod" \;)
+"$renpy/renpy.sh" "$mas" compile >/dev/null 2>&1
 
-cleanup() {
-    rm -rf "$mod"
-    rm -f "$mas/errors.txt" "$mas/traceback.txt" "$mas/compile.log"
-}
-trap cleanup EXIT
-
-"$renpy/renpy.sh" "$mas" compile 2>&1 \
-    | tail -n +2 \
-    | tee "$mas/compile.log" \
-    | perl -sne 'print if (/^\Q$mod\E.*: \w*Warning/ || !/^game\//)' \
-        -- -mod="$(realpath --relative-to="$mod" "$mod")" \
-    | perl -spe 'print $1 if /^\Q$mod\E(.*)/' \
-        -- -mod="$(realpath --relative-to="$mod" "$mod")"
-
-if [ -f "$mas/errors.txt" ] || [ -f "$mas/traceback.txt" ]; then exit 1; fi
+if tail -n +9 "$mas/log/spj.log" | grep -Eq '^.*!ERROR! T_T.*$'; then exit 1; fi
